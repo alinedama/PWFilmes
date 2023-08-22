3540deca4ca2ef89e547e17685aefda6fb9cef9f
Aluno <aluno@etec.sp.gov.br>
2023-08-22 20:46:43 -0300

    Instalação Blazor Bootstrap

--- src/PWFilmes.Presentation/Program.cs
+++ src/PWFilmes.Presentation/Program.cs
@@ -12,12 +12,14 @@ namespace PWFilmes.Presentation
 {
     public class Program
     {
+        //Teste
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazorBootstrap();
 
             await builder.Build().RunAsync();
         }
--- src/PWFilmes.Presentation/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ src/PWFilmes.Presentation/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace PWFilmes.Presentation.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 1 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 2 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 3 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 4 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 5 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 6 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 7 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 8 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 9 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using PWFilmes.Presentation;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\pw\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
+#line 10 "C:\bd\PWFilmes\src\PWFilmes.Presentation\_Imports.razor"
 using PWFilmes.Presentation.Shared;
 
 #line default
